bc1cfededeb7999ea341d0e4f3effe9d70493a7d
eeshaan404 <127168688+eeshaan404@users.noreply.github.com>
2023-03-06 20:59:14 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "villager3WalkFront0":return img`
+c c c b f f f f b c c c c c c c 
+c b f f e e e e f f b c c b c c 
+c f f e e e e e e f f c c c c b 
+f f f f 4 e e e f f f f c c c c 
+f f f 4 4 4 e e f f f f c c c c 
+f f f 4 4 4 4 e e f f f c c b c 
+f 4 e 4 4 4 4 4 4 e 4 f c c c c 
+f 4 4 f f 4 4 f f 4 4 f c c c c 
+f e 4 d d d d d d 4 e f c b c c 
+c f e d d b b d d e f b c c c c 
+b f f e 4 4 4 4 e f f c c c c c 
+e 4 f f 1 2 2 1 f f 4 e c c c c 
+4 d f f f 1 1 f f f d 4 c c c b 
+4 4 f f f f f f f f 4 4 c c c c 
+c b c f f f f f f c c c c b c c 
+c c c f f c c f f b c c c c c c 
+`;
+            case "image1":
+            case "villager3WalkFront1":return img`
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 9 9 9 6 6 9 9 9 9 6 6 9 9 9 6 
+6 9 9 6 6 9 9 c c 9 9 6 6 9 9 6 
+6 9 6 6 9 9 c c 6 9 9 9 6 6 9 6 
+6 6 6 9 9 9 9 c 6 6 9 9 9 6 6 6 
+6 6 9 9 9 9 9 9 6 6 9 9 9 9 6 6 
+6 9 9 9 6 6 6 6 9 6 9 9 c 9 9 6 
+6 9 c 6 6 6 9 9 9 6 9 c c c 9 6 
+6 9 c c c 9 6 9 9 9 6 6 6 c 9 6 
+6 9 9 c 9 9 6 9 6 6 6 6 9 9 9 6 
+6 6 9 9 9 9 6 6 9 9 9 9 9 9 6 6 
+6 6 6 9 9 9 6 6 c 9 9 9 9 6 6 6 
+6 9 6 6 9 9 9 6 c c 9 9 6 6 9 6 
+6 9 9 6 6 9 9 c c 9 9 6 6 9 9 6 
+6 9 9 9 6 6 9 9 9 9 6 6 9 9 9 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
